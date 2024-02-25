017747bac440c0a26f6fda9d00c9dcc3369fb2c1
Myung-jin Ki <myungjinki91@gmail.com>
2024-02-25 15:46:38 +0900

    Modify grid

--- 1_hello.py
+++ 1_hello.py
@@ -5,8 +5,14 @@ root = tk.Tk("Hello World!")
 # Create a label widget
 myLabel1 = tk.Label(root, text="Hello World!")
 myLabel2 = tk.Label(root, text="My name is John Elder")
+myLabel3 = tk.Label(root, text="My name is John Elder")
+myLabel4 = tk.Label(root, text="My name is John Elder")
+myLabel5 = tk.Label(root, text="My name is John Elder")
 
 myLabel1.grid(row=0, column=0)
-myLabel2.grid(row=2, column=3)
+myLabel2.grid(row=1, column=1)
+myLabel3.grid(row=2, column=2)
+myLabel4.grid(row=3, column=3)
+myLabel5.grid(row=100, column=1000)
 
 root.mainloop()
