35b4dfed2f43d371e9a42b8bb902e55303624621
Myung-jin Ki <myungjinki91@gmail.com>
2024-02-26 09:35:30 +0900

    Change pack to grid

--- 2_button.py
+++ 2_button.py
@@ -2,14 +2,18 @@ from tkinter import Tk, Button, Label
 
 
 root = Tk()
+i = 1
 
 
 def myClick():
+    global i
+
+    i += 1
     myLabel = Label(root, text="Look! I clicked a Button!")
-    myLabel.pack()
+    myLabel.grid(column=1, row=i)
 
 
 myButton = Button(root, text="Click Me!", padx=50, pady=50, command=myClick)
-myButton.pack()
+myButton.grid(column=1, row=1)
 
 root.mainloop()
